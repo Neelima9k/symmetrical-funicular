47b2cf07dc9af3de4bf0a3397ace0a9441380126
Neelima9k <116041438+Neelima9k@users.noreply.github.com>
2022-10-18 05:08:51 +0530

    Add files via upload

---
+++ class_constructor.py
@@ -0,0 +1,15 @@
+class Myclass:
+    def __init__(self,a,b):
+       self.a=a
+       self.b=b
+       print("this is the initilazation block")
+    def mymethod1(self):
+        print(self.a)
+        print(self.b)
+objects = Myclass(76,89)
+objects.mymethod1()
+objects1=Myclass(2516,8597)
+print(objects1.b)
+Myclass(860,86).mymethod1()
+print(Myclass(34,56).a)
+print(objects.a)
